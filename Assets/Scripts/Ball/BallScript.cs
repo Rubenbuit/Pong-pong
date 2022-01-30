3c155815ab936afbd7fa046f4a21f8e7cf22e52a
Rubenbuit <56268931+Rubenbuit@users.noreply.github.com>
2022-01-30 17:28:19 +0100

    Changed box Collision
    added new  materials
    changhed collisions
    updated scene

--- Assets/Scripts/Ball/BallScript.cs
+++ Assets/Scripts/Ball/BallScript.cs
@@ -4,44 +4,31 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
-    public float speed = 3;
+    public float speed;
     private Rigidbody2D rb;
     private Vector2 moveAmount;
     private bool startBall = false;
     
-    // Start is called before the first frame update
     void Start()
     {
         startBall = true;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(startBall){
-            Vector2 moveInput = new Vector2(0, 0);
-            moveAmount = moveInput.normalized * speed;
             if(Input.GetKeyDown(KeyCode.Space)){
-                moveAmount = new Vector2(Random.value, Random.value).normalized* speed;
+                moveAmount = new Vector2(0 , 20);
+                rb.AddForce(moveAmount* speed );
                 startBall = false;
             }
         }
     }
 
-    // void OnCollisionEnter2D(Collision other)
-    // {
-    //     Debug.Log(other + "hit item");
-    //     moveAmount = new Vector2( -moveAmount.x ,  -moveAmount.y);
-    // }
-
-private void OnCollisionEnter2D(Collision2D other) {
-     Debug.Log(other + "hit item");
-     moveAmount = new Vector2( -moveAmount.x ,  -moveAmount.y);
-}
-
-    private void FixedUpdate() {
-        Vector2 position = rb.position + moveAmount * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    private void OnCollisionEnter2D(Collision2D other) {
+        moveAmount = new Vector2 (moveAmount.x , (moveAmount.y * -1));
+        rb.AddForce( moveAmount*speed);
     }
 }
+
