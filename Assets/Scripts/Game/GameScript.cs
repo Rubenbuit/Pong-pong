4fd430a74aff8d5b0285dc5c10e16adec253fdaa
Rubenbuit <56268931+Rubenbuit@users.noreply.github.com>
2022-02-12 16:09:37 +0100

    Show lives at start of game
    not working perfect yet because you have a max of lives

--- Assets/Scripts/Game/GameScript.cs
+++ Assets/Scripts/Game/GameScript.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class GameScript : MonoBehaviour
 {
     public int lives = 3;
+    public int maxLives = 5;
     public int score = 0;
-
+    public GameObject[] livesSprite;
+    
     // Start is called before the first frame update
+    public void Start() {
+        int tempLives = lives;
+        for(int i = 0 ; i < livesSprite.Length ; i ++) {
+            if(tempLives > 0) {
+                livesSprite[i].SetActive(true);
+                tempLives --;
+            } else {
+                livesSprite[i].SetActive(false);
+            }
+        }
+    }
+
+        void Update()
+    {
+    }
 
     public void AddScore(int score)
     {
         score += score;
     }
+
+    private void DrawLives(){
+
+    }
+    
 }
